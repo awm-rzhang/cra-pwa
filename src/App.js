9de765dd6f79f73f0c214e43fb4b6370266c9c1a
Ryan Zhang <rzhang@smartshelf.com>
2022-09-21 18:58:52 -0700

    add cam

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
 
-function App() {
+const App = () => {
+  const launchCam = () => {
+    console.log("hello")
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img onClick={launchCam} src={process.env.PUBLIC_URL + '/camera.png'} alt="logo" />
+        <input
+          accept="image/*"
+          style={{ display: 'none' }}
+          id="icon-button-file"
+          type="file"
+          capture="environment"
+        //onChange={(e) => handleCapture(e.target)}
+        />
+
+        <label htmlFor="icon-button-file">
+          <button
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            take photo
+          </button>
+        </label>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/products",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
